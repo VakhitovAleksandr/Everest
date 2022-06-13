01a92489a18b0253c8bc0045db9e160cfdd63bcc
Vakhitov Aleksandr <ale.vakhitov@gmail.com>
2022-06-13 18:22:57 +0500

    13.06.22

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-let droDownMEnu = function () {
-  let menuBurger = document.querySelector(".burger-menu");
-  let menuMobile = document.querySelector(".menu-mobile");
 
-  menuBurger.addEventListener("click", () => {
-    menuBurger.classList.toggle("active");
-    menuMobile.classList.toggle("active");
-  });
-};
-droDownMEnu();
