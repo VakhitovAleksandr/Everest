e0ed2792d70ad7fbcf955a93d0addf649a81740f
Vakhitov Aleksandr <ale.vakhitov@gmail.com>
2022-06-14 11:04:56 +0500

    13.06.22

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let showHideMenu = function () {
+  const menuDrop = document.querySelector(".menu_top");
+  const burgerBtn = document.querySelector(".burger");
 
+  burgerBtn.addEventListener("click", () => {
+    menuDrop.classList.toggle("open");
+    burgerBtn.classList.toggle("open");
+  });
+};
+
+showHideMenu();
